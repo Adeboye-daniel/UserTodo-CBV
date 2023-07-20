28c814c36b99c40eb93e7611bb9b94f32d707ae2
Daniel <adeboyed93@gmail.com>
2023-07-20 14:36:33 +0100

    test

--- todo/settings.py
+++ todo/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 't0dk67bvw8fdmuucvcrv@ppcj-uvb!joq40s6r9=va+)lbmoy&'
+SECRET_KEY_NEW = 't0dk67bvw8fdmuucvcrv@ppcj-uvb!joq40s6r9=va+)lbmoy&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
